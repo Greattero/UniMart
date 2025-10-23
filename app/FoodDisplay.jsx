01cc3311a2697af8b731d855ff9a0327d8abf92f
Foster Ametepey <122822016+Greattero@users.noreply.github.com>
2025-10-23 00:00:55 +0000

    Updated

--- app/FoodDisplay.jsx
+++ app/FoodDisplay.jsx
@@ -1,17 +1,32 @@
+import {
+  Montserrat_400Regular,
+  Montserrat_600SemiBold,
+  Montserrat_700Bold,
+  useFonts,
+} from "@expo-google-fonts/montserrat";
+import Octicons from '@expo/vector-icons/Octicons';
 import React, { useEffect } from "react";
 import { FlatList, Image, Pressable, ScrollView, StyleSheet, Text, View } from "react-native";
 
+
 export default function FoodDisplay(){
+
+   const [fontsLoaded] = useFonts({
+    Montserrat_400Regular,
+    Montserrat_600SemiBold,
+    Montserrat_700Bold,
+    });
+
 
     const [rice, setRice] = React.useState([]);
 
     useEffect(()=>{
     const fetchedFoods = [
-      { id: '1', name: 'Burger', price: '₵25', image: require("../assets/images/burger.jpg")},
-      { id: '2', name: 'Pizza', price: '₵40', image: require("../assets/images/burger.jpg") },
-      { id: '3', name: 'Burger', price: '₵25', image: require("../assets/images/burger.jpg")},
-      { id: '4', name: 'Pizza', price: '₵40', image: require("../assets/images/burger.jpg") },
-      { id: '5', name: 'Burger', price: '₵25', image: require("../assets/images/burger.jpg")},
+      { id: '1', name: 'Burger', price: '₵25', image: require("../assets/images/landjollof.jpg")},
+      { id: '2', name: 'Pizza', price: '₵40', image: require("../assets/images/oilrice.jpg") },
+      { id: '3', name: 'Burger', price: '₵25', image: require("../assets/images/landjollof.jpg")},
+      { id: '4', name: 'Pizza', price: '₵40', image: require("../assets/images/oilrice.jpg") },
+      { id: '5', name: 'Burger', price: '₵25', image: require("../assets/images/landjollof.jpg")},
       { id: '6', name: 'Pizza', price: '₵40', image: require("../assets/images/jollof.jpg") },
       { id: '7', name: 'Burger', price: '₵25', image: require("../assets/images/burger.jpg")},
       { id: '8', name: 'Pizza', price: '₵40', image: require("../assets/images/burger.jpg") },
@@ -24,6 +39,10 @@ export default function FoodDisplay(){
     setRice(fetchedFoods);
 
     },[])
+
+    if (!fontsLoaded) {
+      return null; // or <ActivityIndicator />
+    }
 
 
     return(
@@ -114,6 +133,30 @@ export default function FoodDisplay(){
                 )}
                 />
 
+                <Text style={styles.foodHeader}>All Resturants</Text>
+
+                <FlatList
+                data={rice}
+                vertical
+                scrollEnabled={false}  // 👈 add this
+                showsHorizontalScrollIndicator={false}
+                keyExtractor={(item)=>item.id}
+                contentContainerStyle={{padding: 10}}
+                renderItem={({item}) => (
+                    <View>
+                        <View style={styles.resturantCard}>
+                            <Image source={item.image} style={styles.resturantImage}/>
+                        </View>
+
+                            <Text style={styles.resturantName}>Jesi Dish</Text>
+                        <View style={styles.starRating}>    
+                            <Octicons name="star-fill" size={13} color="orange" />
+                            <Text style={styles.rateDigit}>5.0</Text>       
+                        </View>              
+                    </View>
+                )}
+                />
+
             </View>
 
         </ScrollView>
@@ -177,6 +220,34 @@ const styles = StyleSheet.create({
     color: "white",
     right: 7,
     top: 5
+  },
+  resturantCard:{
+    width: 340,
+    height: 140,
+    borderRadius: 10
+
+  },
+  resturantImage:{
+    height:140,
+    width: 340,
+    marginLeft:0,
+    borderRadius: 10
+
+  },
+  resturantName:{
+    marginTop: 5,
+    fontSize: 18,
+    fontFamily: "Montserrat_600SemiBold",
+  },
+  starRating:{
+    marginBottom: 10,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  rateDigit:{
+    marginLeft:5,
+    color: "orange",
+    fontFamily: "Montserrat_700Bold",
   }
   
 });
